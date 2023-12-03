d9a8af928a999d0cf2cea3cad9078a1d784cd7be
Sandeep Anand <sandeep.maxcraze@gmail.com>
2023-12-03 13:14:08 -0800

    working binary

---
+++ README.md
@@ -0,0 +1,7 @@
+### Simple Grep CLI App Build with Rust
+
+A simple cli app build using rust, to grep a user input pattern in a user input file
+
+```sh
+simplegrep foostring path_to_text_file
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+use clap::Parser;
 
-    println!("pattern {:?}, {:?}", pattern, path);
+/// Search for a pattern in a file and display the lines that contain it
+#[derive(Parser, Debug)]
+struct Cli {
+    /// The pattern to look for
+    _pattern: String,
+    
+
+    /// The path of the file to read
+    _path: std::path::PathBuf,
+}
+
+
+fn main() {
+    let _pattern = std::env::args().nth(1).expect("no pattern given");
+    let _path = std::env::args().nth(2).expect("no path given");
+    
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args._path).expect("could not read the file provided!");
+    
+    for line in content.lines() {
+        if line.contains(&args._pattern) {
+            println!("{}", line);
+        }
+    }
+    println!("pattern {:?}, {:?}", args._pattern, args._path);
 }
