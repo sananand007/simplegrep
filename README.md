b72f2e7aadba54aa2af4a0b0e51e3d4e5236ab37
Sandeep Anand <sandeep.maxcraze@gmail.com>
2023-12-29 04:55:34 -0800

    add word count functionalities and clean output

--- README.md
+++ README.md
@@ -2,14 +2,64 @@
 
 A simple cli app build using rust, to grep a user input pattern in a user input file
 
+Functionalities 
+- gives out the word count
+- checks presence of a user input pattern
+
 ```sh
 <path/to/bin/>simplegrep foostring path_to_text_file
 ```
 
-## Improvments and Next goals
+## Current Output 
+```
+cargo run -- dog ~/Public/testArtifacts/testSimplegrep.txt
+   Compiling simplegrep v0.1.0 (/Users/wano/Public/code/buildWithRust/simplegrep)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.31s
+     Running `target/debug/simplegrep dog /Users/wano/Public/testArtifacts/testSimplegrep.txt`
+pattern "dog" in "/Users/wano/Public/testArtifacts/testSimplegrep.txt" true
+######################################
+on:1
+and:2
+good:2
+back:3
+to:2
+The:11
+house:1
+its:1
+nice:2
+when:1
+then:1
+ran:1
+very:1
+fat:1
+he:1
+jumped:2
+smiling:1
+were:1
+the:8
+fox:11
+at:3
+jumping:1
+now:1
+landed:1
+over:1
+dog:8
+A:1
+struck:1
+returned:2
+angry:1
+was:5
+not:1
+a:1
+limped:1
+knew:2
+forest:1
+######################################
+```
+
+## Improvments
 - Use `BufReader` to improve read speeds for the input document
-- Use a cleaner output format to show the results
-- It will be interesting to understand statistics of the pattern
 - Can we do more than 1 pattern as an input
 - Can we do a directory of Files as an input (as well keeping a file as an input)
 - Add some better CLI documnets such as `--help` and `--version`
+- Get a structured and formatted output, based on kind of output needed
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
-use std::io::BufReader;
-use std::fs::File;
+use std::collections::HashMap;
+
 
 /// Search for a pattern in a file and display the lines that contain it
 #[derive(Parser, Debug)]
@@ -13,18 +13,33 @@ struct Cli {
     _path: std::path::PathBuf,
 }
 
-
 fn main() {
     let _pattern = std::env::args().nth(1).expect("no pattern given");
     let _path = std::env::args().nth(2).expect("no path given");
     
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args._path).expect("could not read the file provided!");
-    
+    let mut check_pattern = false; 
+    let mut high_freq_wordmap: HashMap<String, i32> = HashMap::new();
+    // This can be dynamically done
+    //let high_freq_words = vec!["dog", "fox", "nice", "good"];
     for line in content.lines() {
         if line.contains(&args._pattern) {
-            println!("{}", line);
+            check_pattern = true;
         }
+        for word in line.split_whitespace() {
+            {
+                let mut temp_word = word.to_string(); 
+                temp_word.retain(|temp_word| !r#"()[]{},.*":'"#.contains(temp_word));
+                //println!("temp word {}", temp_word);
+                high_freq_wordmap.entry(temp_word).and_modify(|counter| *counter += 1).or_insert(1);
+            }
+        }
+        }
+    println!("pattern {:?} in {:?} {:?}", args._pattern, args._path, check_pattern);
+    println!("######################################");
+    for (key, val) in high_freq_wordmap.iter() {
+        println!("{key}:{val}");
     }
-    println!("pattern {:?}, {:?}", args._pattern, args._path);
+    println!("######################################");
 }
